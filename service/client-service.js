5ead746d8766c2941639784173a3ca40477a311d
Crisppu <cristobalyat69@hotmail.com>
2022-11-15 21:01:55 -0600

    ej promise funcional

--- service/client-service.js
+++ service/client-service.js
@@ -22,26 +22,41 @@ const crearNuevaLinea = (nombre, email) => {
             </button>
          </li>
       </ul>
-      </td>`
-   ;
-
+      </td>`;
   linea.innerHTML = contenido;
   return linea;
 };
 const table = document.querySelector("[data-table]");
 
-// TODO: comunicacion entre el frontend y el backend
-const http = new XMLHttpRequest();
+function listaDeClientes() {
+  const promise = new Promise((resolve, reject) => {
+    // comunicacion entre el frontend y el backend
+    const http = new XMLHttpRequest();
 
-http.open("GET", "http://localhost:3000/perfil");
+    http.open("GET", "http://localhost:3000/perfil");
 
-http.send();
+    http.send();
 
-http.onload = () => {
-  const data = JSON.parse(http.response);
-  console.log(data);
-  data.forEach((perfil) => {
+    http.onload = () => {
+      const respuesta = JSON.parse(http.response);
+      if(http.status >= 400){
+        reject(respuesta);
+      }else{
+        resolve(respuesta);
+      }
+    };
+  });
+  return promise;
+};
+
+listaDeClientes().then((result) => {
+  console.log(result)
+  result.forEach((perfil) => {
     const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
     table.appendChild(nuevaLinea);
+
   });
-};
+}).catch( function(error){
+alert('ocurrio un error');
+})
+
