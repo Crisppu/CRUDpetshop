c87ae574f89365fa7f6cb3a1665de023b79f8fa9
Crisppu <cristobalyat69@hotmail.com>
2022-11-21 19:53:24 -0600

    implementacion del try catch

--- controllers/actualizar-controller.js
+++ controllers/actualizar-controller.js
@@ -9,20 +9,28 @@ async function obtenerInformacion(){
     }
     const nombre = document.querySelector('[data-nombre]');
     const email = document.querySelector('[data-email]');
-    const perfil = await  clientServices.editarCliente(id);
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-    // clientServices.editarCliente(id).then(function(perfil){//perfil tiene un objeto osea un usuario ya registrado
-    //   nombre.value = perfil.nombre;
-    //   email.value = perfil.email;
-    // });
+    try {
+      const perfil = await  clientServices.editarCliente("id");
+      if(perfil.nombre && perfil.email){
+        nombre.value = perfil.nombre;
+        email.value = perfil.email;
+        // clientServices.editarCliente(id).then(function(perfil){//perfil tiene un objeto osea un usuario ya registrado
+        //   nombre.value = perfil.nombre;
+        //   email.value = perfil.email;
+        // });
+      }else{ throw new Error();}//activamos el error del catch
+
+    } catch (error) {
+      window.location.href = "../screens/error.html";
+    }
+
 };
 obtenerInformacion();
 
 formulario.addEventListener("submit", function(evento){
   evento.preventDefault();
   const url =new URL(window.location);
-    const id = url.searchParams.get('id')
+  const id = url.searchParams.get('id')
   const nombre = document.querySelector('[data-nombre]').value
   const email = document.querySelector('[data-email]').value
   clientServices.actualizarCliente(nombre,email,id).then(function(respuesta){
